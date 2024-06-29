f322c7961addee094b3bb217077bfe1e89ab58fb
heee1 <p1xell@kakao.com>
2024-06-30 04:16:14 +0900

    [Feat] 현재페이지에 대한 인디케이터 기능 추가
    - 현재페이지가 어디인지 확인할 수 있도록 아이콘으로 표시해주는
      인디케이터 기능을 추가하였습니다.
    - usePathname(); 을 사용하였으며 이는 "use client"를 필요로합니다.
      즉, client에서만 작동합니다.
    - usePathname()은 현재 페이지의 경로를 리턴하는데, 이를 path로 받았고
      path가 /이면 Home에 /about-us면 About Us에 깃발이 생기도록 하였습니다.

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,14 +1,20 @@
+"use client"
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+    // "use client가 필요함, client에서만 작동"
+    const path = usePathname(); 
+    console.log(path);
     return (
         <nav>
             <ul>
                 <li>
-                    <Link href="/">Home</Link>
+                    <Link href="/">Home</Link> {path === "/" ? "🚩" : ""}
                 </li>
                 <li>
-                    <Link href="/about-us">About Us</Link>
+                    <Link href="/about-us">About Us</Link> {path === "/about-us" ? "🚩" : ""}
                 </li>
             </ul>
         </nav>
